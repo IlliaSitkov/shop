12c72d8d98da98be5d22a46c6384ba2c19218320
Iryna <irama05@outlook.com>
2022-05-03 13:11:44 +0300

    1 query fixed

--- src/main/java/com/databases/shop/repositories/CategoryRepository.java
+++ src/main/java/com/databases/shop/repositories/CategoryRepository.java
@@ -107,15 +107,10 @@ public interface CategoryRepository extends JpaRepository<Category, Long> {
                                             "FROM product\n" +
                                             "WHERE category_fk = category.id)))" +
              "AND (SELECT COALESCE(SUM(prod_quantity),0)\n" +
-                  "FROM product_in_order\n" +
-                  "WHERE order_id IN(\n" +
-                    "SELECT order_id\n" +
-                    "FROM order_t\n" +
-                    "WHERE status = 'DONE'\n" +
-                    "AND product_articul IN(\n" +
-                        "SELECT articul\n" +
-                        "FROM product\n" +
-                        "WHERE category_fk = category.id))) = \n" +
+                  "FROM product INNER JOIN product_in_order pio ON product.articul = pio.product_articul\n" +
+                      "INNER JOIN order_t ON order_t.id = pio.order_id\n" +
+                  "WHERE status = 'DONE'\n" +
+                  "AND category_fk = category.id) = \n" +
                     "(SELECT COALESCE(MAX(COALESCE(products_quantity,0)),0)\n" +
                     "FROM category LEFT OUTER JOIN (\n" +
                     "    SELECT category_fk, SUM(prod_quantity) AS products_quantity\n" +
